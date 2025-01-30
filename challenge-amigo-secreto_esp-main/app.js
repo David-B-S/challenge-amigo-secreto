292f50ec53b63843398debceb536da6d594e1573
David Barrera <davidsantiagobarrera0@gmail.com>
2025-01-29 21:30:57 -0500

    Create fuction ti generate random number to choose a friend, also clear the code to be simple to read

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,47 +1,50 @@
 // Declaración de variables
 
 let listaDeAmigos = [];
+let lista ;
 
-function agregarAmigo() {
-    
+//Creación de Funciones
+
+function agregarAmigo() {    
     let nombreAmigo = document.querySelector('#amigo').value;
-
-    if ( nombreAmigo === ""){
-
-        alert ("Por favor, inserte un nombre.");
-
-    } else {
-
+    if ( nombreAmigo === ""){        
+        alert ("Por favor, inserte un nombre.");        
+    } else {       
         listaDeAmigos.push(nombreAmigo);
-        console.log(listaDeAmigos);
         limpiarCampoNombre ('#amigo', '');
         agregarNombres(listaDeAmigos);
-        
     }
-    
-    
     return;
 }
 
-function limpiarCampoNombre(elemento, texto) {
-    
-    document.querySelector(elemento).value = texto;
-    return;
 
+function limpiarCampoNombre(elemento, texto) {
+    document.querySelector(elemento).value = texto;
+    return;   
+}
+
+function limpiarListaAmigos(){
+    lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = '';
+    return;
 }
 
 function agregarNombres(amigos){
-    
-    let lista = document.querySelector('#listaAmigos');
-    lista.innerHTML = '';
-    
+    limpiarListaAmigos();
     for (let i = 0; i < amigos.length; i++) {
-        
         let nuevoLi = document.createElement('li');
         nuevoLi.textContent = amigos[i];
         lista.appendChild(nuevoLi);
-        console.log(lista);
-        
     }
+    return;
 }
-    
+
+function sortearAmigo(){
+    if (listaDeAmigos == ""){        
+        alert ('No se registra ningún amigo');        
+    }else {
+        limpiarListaAmigos();
+        let amigoSorteado = document.querySelector('#resultado');
+        amigoSorteado.innerHTML = `El amigo secreto sorteado es: ${listaDeAmigos [Math.floor(Math.random()*listaDeAmigos.length)+1]}`;        
+    }    
+}
